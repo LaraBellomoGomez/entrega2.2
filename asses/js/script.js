9c5be571b99314253534e003ba8842f15ca30c41
Lara BG <larabellomogomez@gmail.com>
2026-01-05 19:53:34 +0100

    guardo cambios

--- asses/js/script.js
+++ asses/js/script.js
@@ -2,3 +2,28 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+
+const cantidad = document.querySelector(".cantidad");
+const mas = document. querySelector(".mas");
+const menos = document. querySelector(".menos");
+
+
+
+mas.addEventListener ("click", () => {
+
+    let contador = Number(cantidad.innerHTML)
+
+    contador = contador +1
+    cantidad.innerHTML = contador
+
+})
+
+menos.addEventListener ("click", () => {
+
+    let contador = Number(cantidad.innerHTML)
+
+    contador = contador -1
+    cantidad.innerHTML = contador
+
+})
