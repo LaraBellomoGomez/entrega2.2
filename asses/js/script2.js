ed821ce4e392827b7766ac31039e65c56f438ccd
Lara BG <larabellomogomez@gmail.com>
2026-01-07 08:32:27 +0100

    agregamos segundo ejercicio

---
+++ asses/js/script2.js
@@ -0,0 +1,17 @@
+const inputs = document.querySelectorAll(".completar"); 
+const boton = document.querySelector(".btn");
+const texto = document.querySelector(".texto");
+
+boton.addEventListener("click", () => {
+  // sumamos los valores de los inputs
+  let total = 0;
+  inputs.forEach(input => {
+    total += Number(input.value) || 0; 
+  });
+
+  if (total <= 10) {
+    texto.textContent = "Llevas " + (total) + " stickers.";
+  } else {
+    texto.textContent = "Llevas demasiados stickers.";
+  }
+});
